4781392154cb67f69f2c63cc1ab0f3cc3d27a858
aJIEw <ajiew42@gmail.com>
2026-04-17 13:00:14 +0800

    fix issues when running on android

--- lib/core/util/utils.dart
+++ lib/core/util/utils.dart
@@ -1,6 +1,6 @@
 import 'package:date_format/date_format.dart';
 import 'package:flutter/services.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import 'toast.dart';
