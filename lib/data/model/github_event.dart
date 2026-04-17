5927102b78ec82ad43fc1129835af4a1e1160cf5
aJIEw <ajiew42@gmail.com>
2026-04-18 00:31:02 +0800

    fix event name issue

--- lib/data/model/github_event.dart
+++ lib/data/model/github_event.dart
@@ -1,10 +1,25 @@
-enum GithubEvent {
-  watchEvent,
-  forkEvent,
-  releaseEvent,
-  createEvent,
-  pushEvent,
-  publicEvent,
-  issuesEvent,
-  issueCommentEvent,
+class GithubEvent {
+  final String name;
+
+  const GithubEvent._(this.name);
+
+  static const watchEvent = GithubEvent._('WatchEvent');
+  static const forkEvent = GithubEvent._('ForkEvent');
+  static const releaseEvent = GithubEvent._('ReleaseEvent');
+  static const createEvent = GithubEvent._('CreateEvent');
+  static const pushEvent = GithubEvent._('PushEvent');
+  static const publicEvent = GithubEvent._('PublicEvent');
+  static const issuesEvent = GithubEvent._('IssuesEvent');
+  static const issueCommentEvent = GithubEvent._('IssueCommentEvent');
+
+  static const values = [
+    watchEvent,
+    forkEvent,
+    releaseEvent,
+    createEvent,
+    pushEvent,
+    publicEvent,
+    issuesEvent,
+    issueCommentEvent,
+  ];
 }
--- test/general_test.dart
+++ test/general_test.dart
@@ -1,5 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:jithub_flutter/data/model/github_event.dart';
 
 void main() {
   test('Test Object', () {});
+
+  test('GithubEvent keeps api event names stable', () {
+    expect(GithubEvent.watchEvent.name, 'WatchEvent');
+    expect(GithubEvent.pushEvent.name, 'PushEvent');
+    expect(GithubEvent.issueCommentEvent.name, 'IssueCommentEvent');
+  });
 }
